1c2e889cce229079efbc91a292747caa0a8d6278
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2023-01-03 12:47:48 -0600

    Initial commit

---
+++ src/components/InputText.vue
@@ -0,0 +1,72 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  type: {
+    type: String,
+    required: false,
+    default: 'text'
+  },
+  required: {
+    type: Boolean,
+    default: false
+  },
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+  placeholder: String,
+  id: String,
+  disabled: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+function updateInput(event) {
+  emit("update:modelValue", event.target.value)
+}
+</script>
+
+<template>
+  <div class="input-group w-full flex flex-col z-10 relative mix-blend-lighten">
+    <label
+      :for="id"
+      class="uppercase text-xs absolute -top-1.5 left-6 bg-black px-2 text-slate-400"
+    >
+      {{ label }}
+      <span v-if="required" class="text-violet-400">*</span>
+    </label>
+
+    <input
+      :id="id"
+      :placeholder="placeholder"
+      :aria-placeholder="placeholder"
+      :type="type"
+      :disabled="disabled"
+      :value="modelValue" 
+      class="input-group__input text-base lg:text-lg text-slate-100 w-full rounded-xl px-3 lg:px-4 py-2 lg:py-3 placeholder:text-slate-600 border-2 border-transparent outline-none"
+      @input="updateInput"
+    />
+  </div>
+</template>
+
+<style>
+.input-group__input {
+  border-style: none;
+  border: 0.1em solid transparent;
+  background-image: linear-gradient(#000, #000), linear-gradient(120deg, #f43f5e 0%, #7e22ce 50%, #9f0 100%);
+  background-origin: border-box;
+  background-clip: padding-box, border-box;
+  background-size: 200% 100%;
+  transition: background-position 0.8s ease-out;
+}
+.input-group__input:hover {
+	background-position: 100% 0;
+}
+.input-group__input:focus {
+  outline: 2px solid #ad2b89;
+  outline-offset: 0.1em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <script setup>
+import InputTextarea from '../components/InputTextarea.vue'
+import InputText from '../components/InputText.vue'
 import { inject } from 'vue'
 const $api = inject('$api')
 </script>
 
 <template>
-  <h1 class="text-slate-800 font-light text-2xl text-center mt-12">Welcome</h1>
-  <h2 class="text-center mt-2 text-slate-600">This is a home page served by Vue Router.</h2>
+  <section class="grid min-h-screen place-content-center">
+    <div class="w-80 space-y-6">
+      <InputText type="text" label="Your name" placeholder="Enter your name" required />
+      <InputTextarea label="Text area" />
+    </div>
+  </section>
 </template>
