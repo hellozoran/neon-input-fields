7a3b19448d71f104fee2e54f0f3702688d42742c
Zoran Mitrovic <32069164+hellozoran@users.noreply.github.com>
2023-01-04 12:16:08 -0600

    Added neon logo and link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,17 @@ const $api = inject('$api')
 </script>
 
 <template>
-  <section class="grid min-h-screen place-content-center">
-    <div class="w-80 space-y-6">
+  <section class="grid min-h-screen place-content-center relative">
+    <div class="w-80">
+      <img src="/neon-inputs.png" class="absolute top-1 w-40 left-3" />
+      <a href="https://zoranmitrovic.com" class="text-slate-400 absolute top-8 text-sm right-6 flex items-center gap-1">
+        Read the article
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+        </svg>
+      </a>
       <InputText type="text" label="Your name" placeholder="Enter your name" required />
-      <InputTextarea label="Text area" />
+      <InputTextarea label="Text area" placeholder="Enter your message" class="mt-6" />
     </div>
   </section>
 </template>
